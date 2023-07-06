0896562f713048516138b9b6603882dda3d6c8f3
LastDough <abdurrahman.abdur02@gmail.com>
2023-07-06 23:27:26 +0700

    Write UI test to validate when user tap Add Task (+), the AddTaskActivity displayed.

--- app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
@@ -1,6 +1,36 @@
 package com.dicoding.todoapp.ui.list
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.ui.add.AddTaskActivity
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
 //TODO 16 : Write UI test to validate when user tap Add Task (+), the AddTaskActivity displayed
+@RunWith(AndroidJUnit4ClassRunner::class)
 class TaskActivityTest {
 
+    @Before
+    fun setup(){
+        ActivityScenario.launch(TaskActivity::class.java)
+        Intents.init()
+    }
+
+    @Test
+    fun whenAddTaskClickedDisplayedAddTaskActivity(){
+        onView(withId(R.id.fab)).perform(click())
+        intended(hasComponent(AddTaskActivity::class.java.name))
+        onView(withHint(R.string.title_hint)).check(matches(isDisplayed()))
+        onView(withHint(R.string.description_hint)).check(matches(isDisplayed()))
+    }
 }

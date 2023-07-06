5fee09af1e6fbc8e31d566cd4b5988af31749dbb
LastDough <abdurrahman.abdur02@gmail.com>
2023-07-06 22:06:49 +0700

    Schedule and cancel daily reminder using WorkManager. If notification preference is on, get the earliest active task from the repository and show notification with pending intent.

--- app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
@@ -1,15 +1,22 @@
 package com.dicoding.todoapp.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.dicoding.todoapp.R
 import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.data.TaskRepository
 import com.dicoding.todoapp.ui.detail.DetailTaskActivity
+import com.dicoding.todoapp.utils.DateConverter
 import com.dicoding.todoapp.utils.NOTIFICATION_CHANNEL_ID
+import com.dicoding.todoapp.utils.NOTIFICATION_ID
 import com.dicoding.todoapp.utils.TASK_ID
 
 class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
@@ -32,7 +39,43 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
 
     override fun doWork(): Result {
         //TODO 14 : If notification preference on, get nearest active task from repository and show notification with pending intent
+        val task: Task = TaskRepository.getInstance(applicationContext).getNearestActiveTask()
+        sendNotification(task)
         return Result.success()
+    }
+
+    private fun sendNotification(task: Task) {
+        val pendingIntent = getPendingIntent(task)
+
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val builder = NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+            .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(task.title)
+            .setContentText(
+                applicationContext.getString(
+                    R.string.notify_content,
+                    DateConverter.convertMillisToString(task.dueDateMillis)
+                )
+            )
+            .setAutoCancel(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = channelName
+            builder.setChannelId(NOTIFICATION_CHANNEL_ID)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = builder.build()
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
     }
 
 }
--- app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.notification.NotificationWorker
+import com.dicoding.todoapp.utils.NOTIFICATION_CHANNEL_ID
+import java.util.concurrent.TimeUnit
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -29,6 +35,19 @@ class SettingsActivity : AppCompatActivity() {
             prefNotification?.setOnPreferenceChangeListener { preference, newValue ->
                 val channelName = getString(R.string.notify_channel_name)
                 //TODO 13 : Schedule and cancel daily reminder using WorkManager with data channelName
+                val checkedStatus = newValue as Boolean
+                val data = workDataOf(NOTIFICATION_CHANNEL_ID to channelName)
+                val workRequest = PeriodicWorkRequestBuilder<NotificationWorker>(
+                    1, TimeUnit.DAYS)
+                    .addTag(channelName)
+                    .setInputData(data)
+                    .build()
+                val workManager = WorkManager.getInstance(requireActivity())
+                if (checkedStatus) {
+                    workManager.enqueue(workRequest)
+                } else {
+                    workManager.cancelAllWorkByTag(channelName)
+                }
                 true
             }
 
